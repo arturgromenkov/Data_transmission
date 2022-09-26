f82746600e3c05a2830317531d0f10102675248d
GitGud <36747104+arturgromenkov@users.noreply.github.com>
2022-09-26 20:28:09 +0300

    Change MainActivity1

--- app/src/main/java/com/example/data_transmission/MainActivity.kt
+++ app/src/main/java/com/example/data_transmission/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.data_transmission
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {//Что такое Activity?
     //Activity это одно окно
     override fun onCreate(savedInstanceState: Bundle?) {//Это так называемый цикл жизни Activity
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val settingsbutton: ImageButton=findViewById(R.id.to_settings_button)
+
+        settingsbutton.setOnClickListener()
+
+        {
+            val Intent= Intent(this,SettingsActivity::class.java)
+            startActivity(Intent)
+        }
     }
 }
